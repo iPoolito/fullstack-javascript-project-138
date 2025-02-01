1fd7e0a88bbb2a6e6a57f2e9d284afedb2f0498d
Pool Ortega <poolortegas@gmail.com>
2025-02-01 08:17:58 -0600

    Reject promise

--- src/pageLoader.js
+++ src/pageLoader.js
@@ -91,6 +91,10 @@ const downloadPage = async (pageUrl, outputDirName = '') => {
       log(`🎉 File successfully saved at: ${fullOutputFilename}`)
       return { filepath: fullOutputFilename }
     })
+    .catch(error => {
+      log('🔥 ERROR:', error)
+      return Promise.reject(error)
+    })
   log('WHAT I RETURN AS PROMISE', promise)
   return promise
 }
